32932caed5e605a8fc0b4738f6d55aa204e21e54
k2pme <cmantsila0@gmail.com>
2023-05-12 12:46:03 +0100

    gestion des exceptions du socket

--- data/email.py
+++ data/email.py
@@ -1,12 +1,19 @@
 import smtplib
 from email.mime.text import MIMEText
+from socket import gaierror
 
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(sender, password)
-    smtp_server.sendmail(sender, recipients, msg.as_string())
-    smtp_server.quit()
+    try :
+        smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+        
+    except gaierror :
+        print("Gmail server can't be reached, check your internet connexion :(")
+    
+    else :
+        smtp_server.quit()
