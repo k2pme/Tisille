6c5ca14e4b2e57680ab6ad5c255ace3df677ab48
k2pme <cmantsila0@gmail.com>
2023-05-12 12:02:20 +0100

    deplacement de la fonction dans le fichier email.py

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import smtplib
-from email.mime.text import MIMEText
-
-def send_email(subject, body, sender, recipients, password):
-    msg = MIMEText(body)
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = ', '.join(recipients)
-    smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    smtp_server.login(sender, password)
-    smtp_server.sendmail(sender, recipients, msg.as_string())
-    smtp_server.quit()
-
+from .data.email import send_email
 
 
 send_email("subject", "body", "nzadouapp@gmail.com", "cmantsila0@gmail.com", "mlfxcbzjmsliioaj")
